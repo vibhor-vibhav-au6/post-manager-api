9bb14ef2ce1f2d0d772e5749b137639e72c29d3d
vibhor <vibhorsmiki@gmail.com>
2020-08-29 15:31:37 +0530

    user and post routes creation

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const postRouter = require('./routers/post')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+
+
+app.use(express.json())
+app.use(userRouter)
+app.use(postRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
